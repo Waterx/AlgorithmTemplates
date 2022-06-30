58c7ad6b79fcf7c5176e18713870ede09a90c44d
haiyue <hzx915184874@gmail.com>
2022-06-30 18:14:29 +0800

    change var names

--- 排序/归并排序.py
+++ 排序/归并排序.py
@@ -11,37 +11,37 @@
 def mergeSort(array):
     if len(array) > 1:
 
-        #  r is the point where the array is divided into two subarrays
-        r = len(array)//2
-        L = array[:r]
-        M = array[r:]
-        # print(L, M)
+        # mid is the point where the array is divided into two subarrays
+        mid = len(array)//2
+        L = array[:mid]
+        R = array[mid:]
+        # print(L, R)
         # Sort the two halves
         mergeSort(L)
-        mergeSort(M)
+        mergeSort(R)
 
         i = j = k = 0
 
-        # Until we reach either end of either L or M, pick larger among
-        # elements L and M and place them in the correct position at A[p..r]
-        while i < len(L) and j < len(M):
-            if L[i] < M[j]:
+        # Until we reach either end of either L or R, pick larger among
+        # elements L and R and place them in the correct position at A[p..r]
+        while i < len(L) and j < len(R):
+            if L[i] < R[j]:
                 array[k] = L[i]
                 i += 1
             else:
-                array[k] = M[j]
+                array[k] = R[j]
                 j += 1
             k += 1
 
-        # When we run out of elements in either L or M,
+        # When we run out of elements in either L or R,
         # pick up the remaining elements and put in A[p..r]
         while i < len(L):
             array[k] = L[i]
             i += 1
             k += 1
 
-        while j < len(M):
-            array[k] = M[j]
+        while j < len(R):
+            array[k] = R[j]
             j += 1
             k += 1
 
@@ -55,7 +55,7 @@ def printList(array):
 
 # Driver program
 if __name__ == '__main__':
-    array = [6, 5, 12, 10, 9, 1]
+    array = [6, 5, 12, 1, 10, 9, 1]
 
     mergeSort(array)
 
