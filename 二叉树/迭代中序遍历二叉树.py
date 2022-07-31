f2a50133b87096b427513dafdec8af1c03f899ea
haiyue <hzx915184874@gmail.com>
2022-07-31 16:02:02 +0800

    add comments

--- 二叉树/迭代中序遍历二叉树.py
+++ 二叉树/迭代中序遍历二叉树.py
@@ -1,5 +1,8 @@
 # 技巧：模板中两处while root相同
 
+# 在 BST 中，while root的意义在于：比 root 大的最小值位于 root 右子树的最左子树
+# 参考下 450. 删除二叉搜索树中的节点 https://leetcode.cn/problems/delete-node-in-a-bst/
+
 def midOrder(self, root):
     stack = []
     result = []
